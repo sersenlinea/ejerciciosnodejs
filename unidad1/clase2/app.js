7f5c76a45180fccafe4d4d28eacc648c0486089d
luisNavasArg <lpipnavas@gmail.com>
2022-07-18 15:36:23 -0300

    clases ordenadas unidad 1 y 2

---
+++ unidad1/clase2/app.js
@@ -0,0 +1,8 @@
+console.log('primero');
+console.log('segundo');
+console.log('tercero');
+console.log('primero');
+	setTimeout ( ()=>{ 
+		console.log('segundo');
+}, 10);
+console.log('tercero');
---
+++ unidad2/clase1/app.js
@@ -0,0 +1,9 @@
+console.log("me gusta programar")
+const message=nombre=>console.log(`Bienvenid@ al usuario ${nombre}`)
+message("Pedro Ródriguez")
+// console.log(window)//en el navegador
+// console.log(window); acá ReferenceError: window is not defined
+//console.log(document); //acá también ReferenceError: document is not defined
+
+console.log(__dirname);
+console.log(__filename);
